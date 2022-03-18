98d081680309075ae80d0a80d9ac372a154da592
ramatov-zulfikor <ramatovv@gmail.com>
2022-03-18 17:10:34 +0500

    markup: finished list of the films component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,25 @@
   <the-header></the-header>
   <div class="container">
     <films-random-card></films-random-card>
+    <div class="row">
+      <div class="col-3">
+        <films-list></films-list>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import TheHeader from '@/components/Layout/TheHeader.vue';
 import FilmsRandomCard from '@/components/Films/FilmsRandomCard.vue';
+import FilmsList from '@/components/Films/FilmsList.vue';
 
 export default {
   name: 'App',
   components: {
     TheHeader,
     FilmsRandomCard,
+    FilmsList,
   },
 };
 </script>
---
+++ src/components/Films/FilmsList.vue
@@ -0,0 +1,42 @@
+<template>
+  <base-card>
+    <ul class="film-list">
+      <li class="film-list__item">My Neighbor Totoro</li>
+      <li class="film-list__item">My Neighbor Totoro</li>
+      <li class="film-list__item">My Neighbor Totoro</li>
+      <li class="film-list__item">My Neighbor Totoro</li>
+      <li class="film-list__item">My Neighbor Totoro</li>
+    </ul>
+  </base-card>
+</template>
+
+<script>
+import BaseCard from '@/components/Base/BaseCard.vue';
+
+export default {
+  name: 'FilmsList',
+  components: { BaseCard },
+};
+</script>
+
+<style lang="scss" scoped>
+.film-list {
+  &__item {
+    font-size: 16px;
+    line-height: 22px;
+    color: #101010;
+    cursor: pointer;
+    padding: 10px 0;
+    -webkit-transition: color 0.2s linear;
+    -moz-transition: color 0.2s linear;
+    -ms-transition: color 0.2s linear;
+    -o-transition: color 0.2s linear;
+    transition: color 0.2s linear;
+    border-bottom: 1px solid #a3a3a3;
+
+    &:hover {
+      color: #109ceb;
+    }
+  }
+}
+</style>
